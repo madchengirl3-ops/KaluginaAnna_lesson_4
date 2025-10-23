c4fa7ea634571f9e34d222b555df442836af443a
Anna Kalugina <madchengirl3@gmail.com>
2025-10-23 18:01:11 +0300

    сумма цифр в числе

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-//Подсчет цифр в числе
-function countDigits(num) {
-    const numString = String(num);
-    return numString.length;
+//Сумма цифр в числе
+function sumDigits(num) {
+    let sum = 0;
+    let tempNum = Math.abs(num);
+    while (tempNum > 0) {
+        sum += tempNum % 10;
+        tempNum = Math.floor(tempNum / 10);
+    }
+    return sum;
 }
-console.log(countDigits(123456));
+console.log(sumDigits(1234));
