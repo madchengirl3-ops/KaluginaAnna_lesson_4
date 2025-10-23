a43823e0fef661d0b32669be1904c17e8b392fed
Anna Kalugina <madchengirl3@gmail.com>
2025-10-23 16:42:05 +0300

    простые задания

---
+++ index.js
@@ -0,0 +1,11 @@
+//Подсчет четных чисел
+function countEven(n) {
+    let count = 0;
+    for (let i = 1; i <= n; i++) {
+        if (i % 2 === 0) {
+            count++;
+        }
+    }
+    return count;
+}
+console.log(countEven(10));
